378d6612650e8dc5d344f8da36e0a35105bbe1d9
alenpaul2001 <alenpaul2001@gmail.com>
2020-01-20 11:59:34 +0530

    new

---
+++ salary.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+void main(){
+    char name[100];
+    int number,basic,DA,HR,Total,Deduction,Net;
+
+    printf("Enter The Employ Name : ");
+    scanf("%s",name);
+    printf("Enter The Employ Number : ");
+    scanf("%d",&number);
+    printf("Enter The Basic Pay : ");
+    scanf("%d",&basic);
+	system("clear");
+    DA=(basic*20)/100 ;
+    HR=(basic*10)/100 ;
+    Total=basic+DA+HR ;
+    Deduction=(Total*5)/100 ;
+    Net=Total-Deduction ;
+
+    printf("Employ Name = %s\n",name);
+    printf("Employ Number = %d\n",number);
+    printf("Dearness Allowance[DA] = %d\n",DA);
+    printf("HRA = %d\n",HR);
+    printf("Deduction = %d\n",Deduction);
+    printf("Total Salary = %d\n",Total);
+    printf("Net Salary = %d\n",Net);
+}
+
+
+
+
+
