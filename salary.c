bd641d1a0fd7a7cc88486ac624ec349f9a2a35cf
AlenPaulVarghese <alenpaul2001@gmail.com>
2020-02-10 20:51:17 +0530

    Added Array Programs

---
+++ salary.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+<<<<<<< HEAD
+#include <stdlib.h>
+=======
+>>>>>>> old/master
+void main(){
+    char name[100];
+    int number,basic,DA,HR,Total,Deduction,Net;
+
+    printf("Enter The Employ Name : ");
+    scanf("%s",name);
+    printf("Enter The Employ Number : ");
+    scanf("%d",&number);
+    printf("Enter The Basic Pay : ");
+    scanf("%d",&basic);
+<<<<<<< HEAD
+	system("clear");
+=======
+
+>>>>>>> old/master
+    DA=(basic*20)/100 ;
+    HR=(basic*10)/100 ;
+    Total=basic+DA+HR ;
+    Deduction=(Total*5)/100 ;
+    Net=Total-Deduction ;
+
+    printf("Employ Name = %s\n",name);
+    printf("Employ Number = %d\n",number);
+    printf("Dearness Allowance[DA] = %d\n",DA);
+    printf("HRA = %d\n",HR);
+    printf("Deduction = %d\n",Deduction);
+<<<<<<< HEAD
+    printf("Total Salary = %d\n",Total);
+=======
+>>>>>>> old/master
+    printf("Net Salary = %d\n",Net);
+}
+
+
+
+
+<<<<<<< HEAD
+
+=======
+/* Ethe Ezuthanda
+
+Question : 
+Write a C program to read the employee name employee number and basic pay of an employee.Also,prepare the salary Bill of that employee.
+Salary is calculated as follow.DA is equal to 20 percentage of Basic pay.HR is equal to 10 percentage of Basic pay. Total salary is equal to BasicPay + DA plus HR. Deduction is equal to 5 percentage of total salary. Net salary is equal to total salary -deduction.
+
+*/
+>>>>>>> old/master
