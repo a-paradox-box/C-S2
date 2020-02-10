b872da571bfdb054a93d0a71b20206e06cef65a8
AlenPaulVarghese <alenpaul2001@gmail.com>
2020-02-10 20:38:18 +0530

    Array Programs

---
+++ largest_from_an_array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main(){
+    int big,i,example[10];
+
+    printf("Enter The Elements Of The Array\n");
+
+    for(i=0;i<10;i++)
+        scanf("%d",&example[i]);
+
+    big=example[0];
+
+    for (i=0;i<10; i++){
+        if(big<example[i])
+            big=example[i];
+    }
+    printf("The Biggest Number is %d\n",big);
+}
---
+++ smallest_from_an_array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main(){
+    int small,i,example[10];
+
+    printf("Enter The Elements Of The Array\n");
+
+    for(i=0;i<10;i++)
+        scanf("%d",&example[i]);
+
+    small=example[0];
+
+    for (i=0;i<10; i++){
+        if(small>example[i])
+            small=example[i];
+    }
+    printf("The Smallest Number is %d\n",small);
+}
