323e4b461a43e959b1daaa01abef2ba91049232f
AlenPaulVarghese <38587581+alenpaul2001@users.noreply.github.com>
2020-02-10 20:53:37 +0530

    Update salary.c

--- salary.c
+++ salary.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
-<<<<<<< HEAD
 #include <stdlib.h>
-=======
->>>>>>> old/master
 void main(){
     char name[100];
     int number,basic,DA,HR,Total,Deduction,Net;
@@ -13,11 +10,8 @@ void main(){
     scanf("%d",&number);
     printf("Enter The Basic Pay : ");
     scanf("%d",&basic);
-<<<<<<< HEAD
 	system("clear");
-=======
-
->>>>>>> old/master
+	
     DA=(basic*20)/100 ;
     HR=(basic*10)/100 ;
     Total=basic+DA+HR ;
@@ -29,17 +23,10 @@ void main(){
     printf("Dearness Allowance[DA] = %d\n",DA);
     printf("HRA = %d\n",HR);
     printf("Deduction = %d\n",Deduction);
-<<<<<<< HEAD
     printf("Total Salary = %d\n",Total);
-=======
->>>>>>> old/master
     printf("Net Salary = %d\n",Net);
 }
 
-
-
-
-<<<<<<< HEAD
 
 =======
 /* Ethe Ezuthanda
@@ -49,4 +36,3 @@ Write a C program to read the employee name employee number and basic pay of an 
 Salary is calculated as follow.DA is equal to 20 percentage of Basic pay.HR is equal to 10 percentage of Basic pay. Total salary is equal to BasicPay + DA plus HR. Deduction is equal to 5 percentage of total salary. Net salary is equal to total salary -deduction.
 
 */
->>>>>>> old/master
