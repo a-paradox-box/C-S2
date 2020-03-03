e09ac3a29fb8af8c46b3019188fe081cfef91900
alenpaul2001 <alenpaul2001@gmail.com>
2020-03-03 13:01:13 +0530

    new

---
+++ Transposeofmatrix.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+void main(){
+
+int a[100][100],b[100][100],i,j,r,c;
+
+printf("Enter the row limit of the matrix");
+scanf("%d",&r);
+printf("Enter the column limit of the matrix");
+scanf("%d",&c);
+
+printf("Enter the elements of the matrix");
+for(i=0;i<r;i++)
+	for(j=0;j<c;j++)
+		scanf("%d",&a[i][j]);
+
+for(i=0;i<r;i++)
+	for(j=0;j<c;j++)
+		b[i][j]=a[j][i];
+
+printf("The transpose of matrix : \n");
+
+for(i=0;i<r;i++){
+	printf("\n");
+for(j=0;j<c;j++)	
+	printf("%d\t",b[i][j]);
+}
+}
